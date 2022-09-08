567cb04c2e84b8c896bc6d4f7b2db9aad5c3f673
Gary Crowe <gary.crowe@computacenter.com>
2022-09-08 13:00:39 +0200

    Working solution using flask & python server

--- cl_srv/bingo/README.md
+++
@@ -1,5 +0,0 @@
-# Here is the client code for micro-services using grpc
-Client should use following to import messages
-```bash
-from bingo_pb2 import TicketRequest
-```
--- working_sol/marketplace/marketplace.py
+++ working_sol/marketplace/marketplace.py
@@ -8,9 +8,7 @@ from recommendations_pb2 import BookCategory, RecommendationRequest
 from recommendations_pb2_grpc import RecommendationsStub
 
 import json
-# json.loads(s)
-# Importing the string looks like this
-# [[0, 11, 22, 0, 0, 51, 62, 0, 85], [3, 0, 0, 36, 48, 54, 64, 0, 0], [0, 18, 30, 39, 0, 0, 67, 0, 87]]
+cards = []
 
 app = Flask(__name__)
 
@@ -20,21 +18,26 @@ recommendations_channel = grpc.insecure_channel(
 )
 recommendations_client = RecommendationsStub(recommendations_channel)
 
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 @app.route("/")
 def render_homepage():
+    # Request new bingo board
     recommendations_request = RecommendationRequest(
         user_id=1, category=BookCategory.MYSTERY, max_results=6
     )
+    # Read response
     recommendations_response = recommendations_client.Recommend(
         recommendations_request
     )
-    # This loop appears to work. You are here! Add to long list and send to html print
-    tickets=recommendations_response.recommendations
-    for loop in tickets:
-        print(json.loads(loop.title))
 
-    return render_template(
-        "homepage.html",
-        recommendations=recommendations_response.recommendations,
-    )
+    cards = []
+    for loop in recommendations_response.recommendations:
+        cards.append(json.loads(loop.title))
+
+    # Flatten to list of all numbers
+    # https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists
+    board=sum(flatten(cards), [])
+
+    return render_template( "homepage.html", myboard=board,)
