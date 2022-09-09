68a5b713e633b27903ace37b3b54f71bcb894177
Gary Crowe <gary.crowe@computacenter.com>
2022-09-09 12:03:52 +0200

    Copy Dockerfiles out of way for Openshift testing

---
+++ launch.bash
@@ -0,0 +1,6 @@
+oc new-app https://github.com/gary-crowe/bingo \
+	--context-dir=working_sol/client \
+	--name bingo \
+oc new-app https://github.com/gary-crowe/bingo \
+	--context-dir=working_sol/generator \
+	--name generator \
