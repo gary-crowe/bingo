4b76c0fbe5733dc5ca786269e6befbe34526a117
Gary Crowe <gary.crowe@computacenter.com>
2022-10-10 16:11:29 +0100

    Update launch script for correct test branch

--- launch.bash
+++ launch.bash
@@ -1,18 +1,23 @@
 # These are the "oc" commands to launch the application.
 # Use the template in preference
-
-oc new-app https://github.com/gary-crowe/bingo \
-	--context-dir=working_sol/generator \
+# Card generator
+oc new-app https://github.com/gary-crowe/bingo#latest \
+	--context-dir=code/generator \
 	--name bingo-generator
 
-oc new-app https://github.com/gary-crowe/bingo \
-	--context-dir=working_sol/client \
-	--env RECOMMENDATIONS_HOST=generator \
+# Client App
+oc new-app https://github.com/gary-crowe/bingo#latest \
+	--context-dir=code/client \
+	--env RECOMMENDATIONS_HOST=bingo-generator \
 	--name bingo
 
-oc new-app mysql:8.0~https://github.com/gary-crowe/bingo \
+# MySQL database backend
+oc new-app mysql:8.0~https://github.com/gary-crowe/bingo#latest \
         --name bingo-mysql \
-        --context-dir=working_sol/ \
+        --context-dir=code/ \
         --env MYSQL_DATABASE=games \
         --env MYSQL_USER=gary \
         --env MYSQL_PASSWORD=PassW0rd
+
+# Espose the service
+oc expose svc bingo
