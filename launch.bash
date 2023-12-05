7198faa508fe0c66a78467dcd7675692ddaa5145
Gary Crowe <gary.crowe@computacenter.com>
2023-12-05 13:59:53 +0000

    Updated code for python-38 UBIE image

--- launch.bash
+++ launch.bash
@@ -1,18 +1,18 @@
 # These are the "oc" commands to launch the application.
 # Use the template in preference
 # Card generator
-oc new-app https://github.com/gary-crowe/bingo#latest \
+oc new-app https://github.com/gary-crowe/bingo \
 	--context-dir=code/generator \
 	--name bingo-generator
 
 # Client App
-oc new-app https://github.com/gary-crowe/bingo#latest \
+oc new-app https://github.com/gary-crowe/bingo \
 	--context-dir=code/client \
 	--env GENERATOR_HOST=bingo-generator \
 	--name bingo
 
 # MySQL database backend
-oc new-app mysql:8.0~https://github.com/gary-crowe/bingo#latest \
+oc new-app mysql:8.0~https://github.com/gary-crowe/bingo \
         --name bingo-mysql \
         --context-dir=code/ \
         --env MYSQL_DATABASE=games \
