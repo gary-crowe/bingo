56714a83913aee930c1a18f585f24379893b857b
Gary Crowe <gary.crowe@computacenter.com>
2023-12-05 13:44:43 +0000

    Try different image

--- code/launch.bash
+++ code/launch.bash
@@ -1,16 +1,16 @@
 # These are the "oc" commands to launch the application.
 # Use the template in preference
 
-oc new-app https://github.com/gary-crowe/bingo#latest \
+oc new-app https://github.com/gary-crowe/bingo \
 	--context-dir=code/generator \
 	--name bingo-generator
 
-oc new-app https://github.com/gary-crowe/bingo#latest \
+oc new-app https://github.com/gary-crowe/bingo \
 	--context-dir=code/client \
 	--env GENERATOR_HOST=bingo-generator \
 	--name bingo
 
-oc new-app mysql:8.0~https://github.com/gary-crowe/bingo#latest \
+oc new-app mysql:8.0~https://github.com/gary-crowe/bingo \
         --name bingo-mysql \
         --context-dir=code/ \
         --env MYSQL_DATABASE=games \
